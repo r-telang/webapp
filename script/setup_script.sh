a4a0aac595516dc337a43e353b22018c807a2618
r-telang <telang.r@northeastern.edu>
2023-10-25 15:33:52 -0400

    changes in script-2

--- script/setup_script.sh
+++ script/setup_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Update package lists
 sudo apt-get update
 
@@ -15,22 +14,16 @@ sudo apt install -y nodejs npm unzip
 #creting user group
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+
 # Move webapp.zip and .env to /opt/
 cd /tmp
 sudo mv /tmp/webapp.zip /opt/csye6225/webapp.zip
 
-
-
 # Change directory to /opt/
 cd /opt/csye6225
 
-
-# Purge Mariadb-server
-# sudo apt-get purge mariadb-server
-
 # Update package lists
 sudo apt update
-
 
 # Unzip webapp.zip
 sudo unzip webapp.zip
@@ -46,8 +39,3 @@ sudo systemctl enable csye6225
 sudo systemctl start csye6225
 sudo systemctl restart csye6225
 sudo systemctl stop csye6225
-
-# Install Node.js dependencies
-sudo npm install
-
-
